ca1ab93b1d6810ecb51a5b3800663f80267d759b
Chris Bowdon <c.bowdon@gmail.com>
2012-01-14 09:14:08 +0800

    added two more test cases to solve

--- name-clean-rules-test.rkt
+++ name-clean-rules-test.rkt
@@ -11,83 +11,87 @@
 (define rules (get-rules rules-file))
 
 (define white-list
-    (list   
-     "Season"
-     "McStroke"
-     "MacDonalds"
-     "DeLorean"
-     "S99E99"))
-  
-  (define black-list
-    (list
-     "[test]"
-     "XviD"
-     "TotALLy mAD"
-     "[2009]"
-     "2009"))
-  
-  (define real-names-bad
-    (list
-     "Test Space End "
-     "Wall-E 2009 XviD"
-     "Zombieland.XviD.Blu.Ray"
-     "Family Guy [2009]"
-     "Kiss.Kiss.Bang.Bang.2009.XviD.(cool)"
-     "Dexter 1x01"
-     "Arrested Development [2.03]"
-     "Family Guy [8x10] McStroke"
-     "Family Guy [8.10] McStroke HD"
-     "Family Guy [8.10] McStroke HD XviD"
-     "District9-720p"
-     "spooks [1x01]"
-     "Bridesmaids.2011.BRRip.XviD.Ac3.Feel-Free"
-     "Pirates of the Caribbean On Stranger Tides (2011) DVDRip XviD-MAXSPEED www.torentz.3xforum.ro"
-     "[SomETAgs] Forgot What Program It Was 2011"
-     "Red.Cliff.2009.ENGLiSH.HardSubbed.DVDRip.XViD-DaPoO"
-     "www.huarenmovie.com@【喜羊羊与灰太狼之兔年顶呱呱】"
-     "末日危途.The.Road.2009.DVD-RMVB-人人影视原创听译中文字幕"
-     "三个白痴 3 Idiots -人人影视原创翻译中英双语字幕"
-     "Bad Teacher[2011]R5 XviD-ExtraTorrentRG"
-     "[isoHunt] Huge_Marvel_Comics_Collection.6164385.TPB"
-     "秘社.The.Secret.Circle.S01E01.Chi_Eng.HDTVrip.720X400-YYeTs人人影视"
-     "超级8.Super.8.2011.DVDSCR-RMVB-人人影视原创翻译中英双语字幕"
-     "加勒比海盗4：惊涛怪浪.Pirates.Of.The.Caribbean.On.Stranger.Tides.2011.BD-MP4-人人影视原创翻译中英双语字幕"
-     "Archer.S03E01.Heart.of.Archness.Part1.720p.HDTV.X264.2011-bits1bytes2"     
-     "jonathan.creek.s02e07.black.canary.1998.christmas.special.dvdrip.xvid"
-     "The.Walking.Dead.S02E03.Save.the.Last.One.HDTV.XviD-FQM.[VTV]"
-     "The.Walking.Dead.S02E07.Pretty.Much.Dead.Already.HDTV.XviD-FQM.[VTV]"))
-  
-  (define real-names-good
-    (list
-     "Test Space End"
-     "Wall-E"
-     "Zombieland"
-     "Family Guy"
-     "Kiss Kiss Bang Bang"
-     "Dexter S01E01"
-     "Arrested Development S02E03"
-     "Family Guy S08E10 McStroke"
-     "Family Guy S08E10 McStroke"
-     "Family Guy S08E10 McStroke"
-     "District9"
-     "Spooks S01E01"
-     "Bridesmaids"
-     "Pirates Of The Caribbean On Stranger Tides"
-     "Forgot What Program It Was"
-     "Red Cliff"
-     "喜羊羊与灰太狼之兔年顶呱呱"
-     "末日危途 The Road"
-     "三个白痴 3 Idiots"
-     "Bad Teacher"
-     "Huge Marvel Comics Collection"
-     "秘社 The Secret Circle S01E01"
-     "超级8 Super 8 " ; OMG MASSIVE CHEAT RIGHT HERE
-     "加勒比海盗4：惊涛怪浪 Pirates Of The Caribbean On Stranger Tides"
-     "Archer S03E01 Heart Of Archness Part1"
-     "Jonathan Creek S02E07 Black Canary Christmas Special"
-     "The Walking Dead S02E03 Save The Last One"
-     "The Walking Dead S02E07 Pretty Much Dead Already"))
-  
+  (list   
+   "Season"
+   "McStroke"
+   "MacDonalds"
+   "DeLorean"
+   "S99E99"))
+
+(define black-list
+  (list
+   "[test]"
+   "XviD"
+   "TotALLy mAD"
+   "[2009]"
+   "2009"))
+
+(define real-names-bad
+  (list
+   "Test Space End "
+   "Wall-E 2009 XviD"
+   "Zombieland.XviD.Blu.Ray"
+   "Family Guy [2009]"
+   "Kiss.Kiss.Bang.Bang.2009.XviD.(cool)"
+   "Dexter 1x01"
+   "Arrested Development [2.03]"
+   "Family Guy [8x10] McStroke"
+   "Family Guy [8.10] McStroke HD"
+   "Family Guy [8.10] McStroke HD XviD"
+   "District9-720p"
+   "spooks [1x01]"
+   "Bridesmaids.2011.BRRip.XviD.Ac3.Feel-Free"
+   "Pirates of the Caribbean On Stranger Tides (2011) DVDRip XviD-MAXSPEED www.torentz.3xforum.ro"
+   "[SomETAgs] Forgot What Program It Was 2011"
+   "Red.Cliff.2009.ENGLiSH.HardSubbed.DVDRip.XViD-DaPoO"
+   "www.huarenmovie.com@【喜羊羊与灰太狼之兔年顶呱呱】"
+   "末日危途.The.Road.2009.DVD-RMVB-人人影视原创听译中文字幕"
+   "三个白痴 3 Idiots -人人影视原创翻译中英双语字幕"
+   "Bad Teacher[2011]R5 XviD-ExtraTorrentRG"
+   "[isoHunt] Huge_Marvel_Comics_Collection.6164385.TPB"
+   "秘社.The.Secret.Circle.S01E01.Chi_Eng.HDTVrip.720X400-YYeTs人人影视"
+   "超级8.Super.8.2011.DVDSCR-RMVB-人人影视原创翻译中英双语字幕"
+   "加勒比海盗4：惊涛怪浪.Pirates.Of.The.Caribbean.On.Stranger.Tides.2011.BD-MP4-人人影视原创翻译中英双语字幕"
+   "Archer.S03E01.Heart.of.Archness.Part1.720p.HDTV.X264.2011-bits1bytes2"     
+   "jonathan.creek.s02e07.black.canary.1998.christmas.special.dvdrip.xvid"
+   "The.Walking.Dead.S02E03.Save.the.Last.One.HDTV.XviD-FQM.[VTV]"
+   "The.Walking.Dead.S02E07.Pretty.Much.Dead.Already.HDTV.XviD-FQM.[VTV]"
+   "摩登家庭.Modern.Family.S03E03.Chi_Eng.WEBrip.720X400-YYeTs人人影视"
+   "铁甲钢拳.Real.Steel.2011.DVD-RMVB.中文字幕-深影字幕组"))
+
+(define real-names-good
+  (list
+   "Test Space End"
+   "Wall-E"
+   "Zombieland"
+   "Family Guy"
+   "Kiss Kiss Bang Bang"
+   "Dexter S01E01"
+   "Arrested Development S02E03"
+   "Family Guy S08E10 McStroke"
+   "Family Guy S08E10 McStroke"
+   "Family Guy S08E10 McStroke"
+   "District9"
+   "Spooks S01E01"
+   "Bridesmaids"
+   "Pirates Of The Caribbean On Stranger Tides"
+   "Forgot What Program It Was"
+   "Red Cliff"
+   "喜羊羊与灰太狼之兔年顶呱呱"
+   "末日危途 The Road"
+   "三个白痴 3 Idiots"
+   "Bad Teacher"
+   "Huge Marvel Comics Collection"
+   "秘社 The Secret Circle S01E01"
+   "超级8 Super 8 " ; OMG MASSIVE CHEAT RIGHT HERE
+   "加勒比海盗4：惊涛怪浪 Pirates Of The Caribbean On Stranger Tides"
+   "Archer S03E01 Heart Of Archness Part1"
+   "Jonathan Creek S02E07 Black Canary Christmas Special"
+   "The Walking Dead S02E03 Save The Last One"
+   "The Walking Dead S02E07 Pretty Much Dead Already"
+   "摩登家庭 Modern Family S03E03"
+   "铁甲钢拳 Real Steel"))
+
 (define/provide-test-suite rules-tests
   
   (test-case 
